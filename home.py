c2f0d09655fad213feee4487aabdc545597aa65a
mw002 <mwang526@wisc.edu>
2022-11-12 17:07:10 -0600

    Modular pages

--- home.py
+++ home.py
@@ -1,12 +1,24 @@
 from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
+placeholder = "<!--PageContent-->"
+
+def generatePage(content):
+    with open("header.html") as f:
+        html = f.read()
+    html = html.replace(placeholder, content)
+    return html
 
 @app.route('/')
 def home():
-    with open("index.html") as f:
-        html = f.read()
+    with open("home.txt") as f:
+        content = f.read()
+    html = generatePage(content)
     return html
+
+@app.route('/query.html')
+def showquery():
+    pass
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, threaded=False)
