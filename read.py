b677806daedd255995d415a00734f7aa84273738
mw002 <mwang526@wisc.edu>
2022-11-12 14:12:58 -0600

    test

--- read.py
+++ read.py
@@ -1,4 +1,11 @@
-import camelot
-tables = camelot.read_pdf('dashboard.pdf')
+from tableauscraper import TableauScraper as TS
+url = "https://tableau.wisconsin.edu/views/UW-MadisonDeansList/ListDashboard"
 
-print(tables[0].df)
+ts = TS()
+ts.loads(url)
+workbook = ts.getWorkbook()
+
+for t in workbook.worksheets:
+    print(f"worksheet name : {t.name}") #show worksheet name
+    print(t.data) #show dataframe for this worksheet
+
